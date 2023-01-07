48ecf5333b3044b36ff7e140d47cb9e89005409b
LORENZO <lorenzobaumgratz3@gmail.com>
2023-01-06 22:19:59 -0300

    Envio do pedido

--- script.js
+++ script.js
@@ -5,22 +5,23 @@ function selecionarbebida(x){
 
     x.classList.add("selecionar");
 
-    let txt=document.querySelector(".bebidas .selecionar h3").innerHTML;
-    alert(txt);
+
     clicarBot();
 }
 
 function selecionarcomida(x){
-    const k=document.querySelector(".linha-pratos .selecionar");
+    const k=document.querySelector(".comida .selecionar");
     if(k!==null)
     k.classList.remove("selecionar");
 
     x.classList.add("selecionar");
 
-    let txt=document.querySelector(".linha-pratos .selecionar h3").innerHTML;
-    alert(txt);
+    
+    
     clicarBot();
 }
+
+l
 
 function selecionarsobremesa(x){
     const k=document.querySelector(".sobremesa .selecionar");
@@ -28,19 +29,18 @@ function selecionarsobremesa(x){
     k.classList.remove("selecionar");
 
     x.classList.add("selecionar");
+    
 
-    let txt=document.querySelector(".sobremesa .selecionar h3").innerHTML;
-    alert(txt);
     clicarBot();
 }
 
-    function clicarBot(){
-        const x=document.querySelector(".bebidas .selecionar");
-        const y=document.querySelector(".linha-pratos .selecionar");
+function clicarBot(){
+        const l=document.querySelector(".bebidas .selecionar");
+        const m=document.querySelector(".comida .selecionar");
         const z=document.querySelector(".sobremesa .selecionar");
         const botao=document.querySelector("button");
     
-        if(x!==null && y!==null && z!==null){
+        if(l!==null && m!==null && z!==null){
             botao.innerHTML="Fechar pedido";
             botao.classList.remove("cinza");
             botao.classList.add("verde");
@@ -51,6 +51,22 @@ function selecionarsobremesa(x){
         
     }
     
+function enviar(){
+    let celular="5548991323411";
+    
+    var currency3 = document.querySelector(".sobremesa .selecionar .preco").innerHTML;
+    var currency1 = document.querySelector(".comida .selecionar .preco").innerHTML;
+    var currency2 = document.querySelector(".bebidas .selecionar .preco").innerHTML;
+    
+    var total=Number(currency3.replace(/[^0-9.-]+/g,""))+Number(currency2.replace(/[^0-9.-]+/g,""))+Number(currency1.replace(/[^0-9.-]+/g,""));
+    let aux=total.toFixed(2);
+    alert(aux);
 
+    let txt=`Olá, gostaria de fazer o pedido: \nPrato: ${document.querySelector(".comida .selecionar h3").innerHTML}\nBebida: ${document.querySelector(".bebidas .selecionar h3").innerHTML}\nSobremesa: ${document.querySelector(".sobremesa .selecionar h3").innerHTML}\nTotal: ${aux}` ;
+    
+    txt=window.encodeURIComponent(txt);
+    window.open("https://api.whatsapp.com/send?phone=" + celular + "&text=" + txt, "_blank");
+
+}
     
     
